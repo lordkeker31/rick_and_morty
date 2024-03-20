f172cef31061fff12817e335bed2e03548e5d160
lordkeker31 <lordkeker31@gmail.com>
2024-03-20 20:48:34 +0400

    20.03 update 2

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import './Category.css'
 import charapters from "../../assets/charapters.jpg";
 
-function Category({img, descriptionHeader, descriptionBody}) {
+function Category({img, title, description}) { //dataArray
     return(
         <div className="category">
             <div className="category__image_container">
                 <img src={img} alt=""/>
             </div>
             <div className="category__info_container">
-                <h3 className="categiry__info_header">{descriptionHeader}</h3>
-                <p className="category__info_description">{descriptionBody}</p>
+                <h3 className="categiry__info_header">{title}</h3>
+                <p className="category__info_description">{description}</p>
             </div>
         </div>
     )
--- src/data/local/catigories.js
+++ src/data/local/catigories.js
@@ -1,3 +1,26 @@
-import charaptersImage from './assets/charapters.jpg';
-import locationImage from './assets/location.png';
-import epizodesImage from './assets/epizodes.png';
+import charaptersImage from '../../assets/charapters.jpg';
+import locationImage from '../../assets/location.png';
+import epizodesImage from '../../assets/epizodes.png';
+
+let catigories = [
+    {
+        id: 1,
+        image: charaptersImage,
+        title: "Персонажи",
+        description: "Зайди и познакомься со всеми персонажами вселенной"
+    },
+    {
+        id: 2,
+        image: locationImage,
+        title: "Локации",
+        description: "Исследуй все локации. Давай же, не будь занудой!"
+    },
+    {
+        id: 3,
+        image: epizodesImage,
+        title: "Эпизоды",
+        description: "Узнай чуть больше о карте приключений Рика и Морти"
+    }
+]
+
+export default catigories
